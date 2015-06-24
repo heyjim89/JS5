c915789a7f4fd230c24e4e9acf25b5cc223e43ac
Jim D <heyjim89@gmail.com>
2015-06-24 14:29:12 -0600

    Saved functionality working

--- main.js
+++ main.js
@@ -7,7 +7,19 @@ $(document).ready(function() {
 
 	$(".parent-container").on("blur", "textarea", function(event) {
 		event.preventDefault();
+		
+		var newText = $(this).val();
+		
+		// console.log(newText);
+		// console.log($(this).parent().siblings('p'));
+
+        $(this).parent().siblings('p').text(newText);
+
+        // console.log($(this).siblings('p'));
+
+		
 		$(".editable").show();
 		$(this).hide();
+
 	});
 });
