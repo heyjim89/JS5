4f4a4afcf97bde665859da0aa1fa3be10e756bbe
Jim D <heyjim89@gmail.com>
2015-06-26 09:28:37 -0600

    Bonus round: Got xeditable to work but now trying ajax local

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
-$(document).ready(function() {
-	$(".editable").on("click", function() {
-		var textEdited = "<div class='form'><textarea class='edit-textarea'>" + $(this).text().trim() + '</textarea>';
-		$(this).hide().after(textEdited);
-		$(".edit-textarea").focus();
-	});
+// $(document).ready(function() {
+// 	$(".editable").on("click", function() {
+// 		var textEdited = "<div class='form'><textarea class='edit-textarea'>" + $(this).text().trim() + '</textarea>';
+// 		$(this).hide().after(textEdited);
+// 		$(".edit-textarea").focus();
+// 	});
 
-	$(".parent-container").on("blur", "textarea", function(event) {
-		event.preventDefault();
-		
-		var newText = $(this).val();
-		
-		// console.log(newText);
-		// console.log($(this).parent().siblings('p'));
+// 	$(".parent-container").on("blur", "textarea", function(event) {
+// 		event.preventDefault();
 
-        $(this).parent().siblings('p').text(newText);
+// 		var newText = $(this).val();
 
-        // console.log($(this).siblings('p'));
+// 		// console.log(newText);
+// 		// console.log($(this).parent().siblings('p'));
 
-		
-		$(".editable").show();
-		$(this).hide();
+//         $(this).parent().siblings('p').text(newText);
 
+//         // console.log($(this).siblings('p'));
+
+
+// 		$(".editable").show();
+// 		$(this).hide();
+
+// 	});
+// 	$('.editable').editable({
+// 		type: 'text',
+// 		title: 'Enter username',
+// 		success: function(response, newValue) {
+// 			userModel.set('username', newValue); //update backbone model
+// 		}
+// 	});
+// });
+
+$(document).on('ready', function() {
+
+	// Found code to set defaults mode to offline by accessing mode to inline
+	$.fn.editable.defaults.mode = 'inline';
+
+	// Apply x-editable plugin to all items with editable class
+	$('.editable').editable();
+
+	// Trying to get it to save locally......
+	$('.editable').editable({
+		type: 'text',
+		title: 'Enter username',
+		success: function(response, newValue) {
+			userModel.set('username', newValue); //update backbone model
+		}
 	});
 });
