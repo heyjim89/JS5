c9e19e148424bbc64b497f18b60ad5fe0cf3e739
Jim D <heyjim89@gmail.com>
2015-06-26 09:33:51 -0600

    Bonus round: Got xeditable to work and added both to page

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-// $(document).ready(function() {
-// 	$(".editable").on("click", function() {
-// 		var textEdited = "<div class='form'><textarea class='edit-textarea'>" + $(this).text().trim() + '</textarea>';
-// 		$(this).hide().after(textEdited);
-// 		$(".edit-textarea").focus();
-// 	});
+$(document).ready(function() {
+	$(".editable").on("click", function() {
+		var textEdited = "<div class='form'><textarea class='edit-textarea'>" + $(this).text().trim() + '</textarea>';
+		$(this).hide().after(textEdited);
+		$(".edit-textarea").focus();
+	});
 
-// 	$(".parent-container").on("blur", "textarea", function(event) {
-// 		event.preventDefault();
+	$(".parent-container").on("blur", "textarea", function(event) {
+		event.preventDefault();
 
-// 		var newText = $(this).val();
+		var newText = $(this).val();
 
-// 		// console.log(newText);
-// 		// console.log($(this).parent().siblings('p'));
+		// console.log(newText);
+		// console.log($(this).parent().siblings('p'));
 
-//         $(this).parent().siblings('p').text(newText);
+        $(this).parent().siblings('p').text(newText);
 
-//         // console.log($(this).siblings('p'));
+        // console.log($(this).siblings('p'));
 
 
-// 		$(".editable").show();
-// 		$(this).hide();
+		$(".editable").show();
+		$(this).hide();
 
-// 	});
-// 	$('.editable').editable({
-// 		type: 'text',
-// 		title: 'Enter username',
-// 		success: function(response, newValue) {
-// 			userModel.set('username', newValue); //update backbone model
-// 		}
-// 	});
-// });
+	});
+	$('.editable').editable({
+		type: 'text',
+		title: 'Enter username',
+		success: function(response, newValue) {
+			userModel.set('username', newValue); //update backbone model
+		}
+	});
+});
 
 $(document).on('ready', function() {
 
@@ -37,10 +37,10 @@ $(document).on('ready', function() {
 	$.fn.editable.defaults.mode = 'inline';
 
 	// Apply x-editable plugin to all items with editable class
-	$('.editable').editable();
+	$('.xeditable').editable();
 
 	// Trying to get it to save locally......
-	$('.editable').editable({
+	$('.xeditable').editable({
 		type: 'text',
 		title: 'Enter username',
 		success: function(response, newValue) {
